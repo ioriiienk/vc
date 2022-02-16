9b7f6644fce48774421995dc7f8b69d2be2cc91e
Shohel Rana <97423280+ioriiienk@users.noreply.github.com>
2022-02-16 15:35:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+var letter = prompt("Enter A Letter Here");
+
+var a = ("This is a Vowel");
+var b = ("This is a Consonant");
+
+c = letter.toUpperCase();
+
+document.write("'" + c + "'" + "<br>");
+
+switch(c){
+    case "A":
+        document.write(a);
+    break;
+    case "B":
+        document.write(b);
+    break;
+    case "C":
+        document.write(b);
+    break;
+    case "D":
+        document.write(b);
+    break;
+    case "E":
+        document.write(a);
+    break;
+    case "F":
+        document.write(b);
+    break;
+    case "G":
+        document.write(b);
+    break;
+    case "H":
+        document.write(b);
+    break;
+    case "I":
+        document.write(a);
+    break;
+    case "J":
+        document.write(b);
+    break;
+    case "K":
+        document.write(b);
+    break;
+    case "L":
+        document.write(b);
+    break;
+    case "M":
+        document.write(b);
+    break;
+    case "N":
+        document.write(b);
+    break;
+    case "O":
+        document.write(a);
+    break;
+    case "P":
+        document.write(b);
+    break;
+    case "Q":
+        document.write(b);
+    break;
+    case "R":
+        document.write(b);
+    break;
+    case "S":
+        document.write(b);
+    break;
+    case "T":
+        document.write(b);
+    break;
+    case "U":
+        document.write(a);
+    break;
+    case "V":
+        document.write(b);
+    break;
+    case "W":
+        document.write(b);
+    break;
+    case "X":
+        document.write(b);
+    break;
+    case "Y":
+        document.write(b);
+    break;
+    case "Z":
+        document.write(b);
+    break;
+    default:
+        document.write("This is not a letter");
+}
